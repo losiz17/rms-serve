a9c0e4376d8629f7049a76171651ce6ad35418da
樋口 翔 <higuchisyo@LAB-N1695.local>
2019-09-19 11:20:28 +0900

    tag serviceの追記

--- src/main/java/io/intern/rms/services/TagService.java
+++ src/main/java/io/intern/rms/services/TagService.java
@@ -1,4 +1,14 @@
 package io.intern.rms.services;
 
+import io.intern.rms.domain.Tag;
+import io.intern.rms.repositories.TagRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class TagService {
+    @Autowired
+    private TagRepository tagRepository;
+
+    public Iterable<Tag> findAllTag(String tagName){
+        return tagRepository.findAllByTagName(tagName);
+    }
 }
