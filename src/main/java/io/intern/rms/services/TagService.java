c73587d4d71593a3356000e8b419205b067b69e1
樋口 翔 <higuchisyo@LAB-N1695.local>
2019-09-19 12:22:33 +0900

    簡易なテストデータ

--- src/main/java/io/intern/rms/services/TagService.java
+++ src/main/java/io/intern/rms/services/TagService.java
@@ -4,6 +4,8 @@ import io.intern.rms.domain.Tag;
 import io.intern.rms.repositories.TagRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import javax.annotation.PostConstruct;
+
 public class TagService {
     @Autowired
     private TagRepository tagRepository;
@@ -11,4 +13,20 @@ public class TagService {
     public Iterable<Tag> findAllTag(String tagName){
         return tagRepository.findAllByTagName(tagName);
     }
+
+    @PostConstruct
+    public void init() {
+        Tag d1 = new Tag();
+        d1.setTagName("Java");
+        tagRepository.save(d1);
+
+        Tag d2 = new Tag();
+        d2.setTagName("Python");
+        tagRepository.save(d2);
+
+        Tag d3 = new Tag();
+        d1.setTagName("Ruby");
+        tagRepository.save(d3);
+    }
+
 }
